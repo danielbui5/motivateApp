62ea788942541df8ccf9bd1c335e8d12e3187f9d
Daniel Bui <danielngn5@gmail.com>
2024-03-14 22:39:23 +1100

    Added dateTimePicker module

--- src/selectSpecifiedTimes.js
+++ src/selectSpecifiedTimes.js
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import { Text, View } from 'react-native';
+import DatePicker from 'react-native-date-picker'
+
+import styles from "./styles";
 
 function SelectSpecifiedTimes({ navigation }) {
     const [nextPage, setNextPage] = useState(false);
+    const [date, setDate] = useState(new Date())
 
     return (
         <View style={{flexDirection: 'column', alignItems: 'center'}}>
